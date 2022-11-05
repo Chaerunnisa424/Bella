116ab81a7e9a643a7abf425ece2a21581e8824af
Chaerunnisa Salsabila <chaesb04@gmail.com>
2022-11-05 12:24:39 +0700

    Okeoke

---
+++ src/Array.java
@@ -0,0 +1,20 @@
+public class Array {
+    public static double[] Hitung(int s, String[] Kalimat) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] number = new int[4];
+        int number1[];
+
+        //memasukan value ke dalam array
+        number[0] = 6;
+        number[3] = 67;
+
+        System.out.println("Panjang array adalah = " +number.length);
+
+        double[] angka_saya={3,4,2,0,5,10,9,8};
+        System.out.println("Panjang array angka saya adalah " +angka_saya.length);
+
+    }
+}
